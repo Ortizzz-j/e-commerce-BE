adec0770e00ab8f33d6054fdf28d01f326d1f901
ortiz MJ <ortizzzj99@gmail.com>
2023-05-20 07:31:03 -0700

    created the models and started the api routes

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [Product]
+    });
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk({ where: {id: req.params.id}, include: [Product] })
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
